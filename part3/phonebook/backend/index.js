91ea2dbbdf76096c76dc20642faf1c7f8f18b8e3
Peter Tolsma <pejato@umich.edu>
2024-04-16 18:06:05 -0400

    Exercises 3.15-3.18

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -17,34 +17,42 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
   const now = new Date().toLocaleString();
-  Person.find({}).then((result) => {
-    response.send(
-      `<p>Phonebook has info for ${result.length} people</p><p>${now}</p>`
-    );
-  });
+  Person.find({})
+    .then((result) => {
+      response.send(
+        `<p>Phonebook has info for ${result.length} people</p><p>${now}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then((result) => {
-    response.json(result);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  Person.findById(id).then((result) => {
-    response.json(result);
-  });
+  Person.findById(id)
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  Person.deleteOne({ _id: id }).then(() => response.status(204).end());
+  Person.findByIdAndDelete(id)
+    .then(() => response.status(204).end())
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name) {
@@ -61,13 +69,41 @@ app.post("/api/persons", (request, response) => {
     name: body.name,
     number: body.number,
   });
-  person.save().then((result) => {
-    response.json(result);
-  });
+  person
+    .save()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
+});
+
+// MARK - Error Handling
 
 app.use((request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
+});
+
+app.use((error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
 });
 
 const PORT = process.env.PORT || 3001;
