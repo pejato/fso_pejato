9615468c00f7e71f00345fd2343dca1e375c62c6
Peter Tolsma <pejato@umich.edu>
2024-04-18 09:38:14 -0400

    Add User model, allow creating and getting all Users

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const { MONGODB_URI } = require('./utils/config');
 const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
+const { errorHandler, unknownEndpoint } = require('./utils/middleware');
 
 const app = express();
 
@@ -13,5 +15,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 module.exports = app;
