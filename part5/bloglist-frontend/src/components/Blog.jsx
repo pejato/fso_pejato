a80966387991ddd6309547b8de90645b6166f9d5
Peter Tolsma <pejato@umich.edu>
2024-04-20 14:49:27 -0400

    Show blog details if expanded

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Blog({ blog }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const buttonText = isExpanded ? 'Hide' : 'View';
+  const expandedContent = (
+    <>
+      <div>
+        <a href={`//${blog.url}`} target="_blank" rel="noreferrer noopener">
+          {blog.url}
+        </a>
+      </div>
+      <div>
+        Likes {blog.likes}
+        <button>like</button>
+      </div>
+      <div>Uploaded by {blog.user.name}</div>
+    </>
+  );
+
   return (
-    <div>
-      {blog.title} {blog.author}
+    <div className="blog-entry">
+      <b>
+        {blog.title} by {blog.author}
+      </b>
+      <button
+        className="expand-button"
+        type="button"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {buttonText}
+      </button>
+      {isExpanded && expandedContent}
     </div>
   );
 }
