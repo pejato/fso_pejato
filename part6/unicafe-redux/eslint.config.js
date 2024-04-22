2ece60a85dc6e40b304e864ac2b952054235634c
Peter Tolsma <pejato@umich.edu>
2024-04-21 21:40:14 -0400

    [part6][unicafe-redux]: Ex 6.1 & 6.2

---
+++ part6/unicafe-redux/eslint.config.js
@@ -0,0 +1,55 @@
+import globals from 'globals';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
+
+export default [
+  { files: ['**/*.js', '**/*.jsx'] },
+  ...compat.extends('airbnb'),
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  {
+    rules: {
+      'no-alert': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-underscore-dangle': 'off',
+      'default-param-last': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-named-as-default': 'off',
+      'react/button-has-type': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/display-name': 'error',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            'vite.config.js',
+            'eslint.config.js',
+            'test*.js',
+            '**/*{.,_}{test,spec}.{js,jsx}',
+          ],
+        },
+      ],
+    },
+  },
+];
