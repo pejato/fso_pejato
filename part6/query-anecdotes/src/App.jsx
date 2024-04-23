c50708baee9cc635c729ba2526224a7002c2a114
Peter Tolsma <pejato@umich.edu>
2024-04-23 15:54:17 -0400

    [part6][query-anecdotes]: Fetch anecdotes using React Query

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,18 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
+import { getAll } from "./requests";
 
 const App = () => {
   const handleVote = (anecdote) => {
     console.log("vote");
   };
 
-  const anecdotes = [
-    {
-      content: "If it hurts, do it more often",
-      id: "47145",
-      votes: 0,
-    },
-  ];
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: () => getAll(),
+  });
+
+  if (isError) {
+    return (
+      <div>Anecdotes are not available due to a problem with the server</div>
+    );
+  } else if (isLoading || !data) {
+    return <div>Loading anecdotes...</div>;
+  }
+
+  const anecdotes = data;
 
   return (
     <div>
