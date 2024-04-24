1fd4c75b4321956dc8846311870af3be4319680c
Peter Tolsma <pejato@umich.edu>
2024-04-24 12:14:40 -0400

    [part7][ultimate-hooks] Modularize, add prettier

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export const useField = (type) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([]);
+
+  // ...
+
+  const create = (resource) => {
+    // ...
+  };
+
+  const service = {
+    create,
+  };
+
+  return [resources, service];
+};
